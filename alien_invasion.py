688c1ef1fabe4483a17050f4fffda2e283fb6e9b
JaqueDay <jqr.day@gmail.com>
2020-06-16 14:09:40 +0100

    chapter 1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,27 @@
 """
 Alien Invasion
 """
+import sys
 
 import pygame
+
 from settings import Settings
 
 
 class AlienInvasion:
-    """ Overall class to manage game assets and behaviour."""
+    """Overall class to manage game assets and behavior."""
 
-    def __int__(self):
+    def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-             (self.settings.screen_width, self.settings.screen_height))
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
-        """Starts the main loop for the game."""
+        """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
@@ -29,11 +31,11 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_colour)
 
-            # Make the most recently drawn screen visible
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # Make a game instance, and run the game
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -11,4 +11,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_colour = (230,230,230)
+        self.bg_colour = (230, 230, 230)
